977b7cc0e7bd845e3365fc7596dab6ff868c2b00
Ali Taheri <ali.taheri.m@gmail.com>
2016-05-24 13:00:29 +0430

    Provide source mapping for tests

--- testSetup.js
+++ testSetup.js
@@ -1,11 +1,14 @@
+require('source-map-support').install({
+  handleUncaughtExceptions: false,
+  environment: 'node',
+});
+
 const jsdom = require('jsdom').jsdom;
-const exposedProperties = ['window', 'navigator', 'document'];
 
 global.document = jsdom('');
 global.window = document.defaultView;
 Object.keys(document.defaultView).forEach((property) => {
   if (typeof global[property] === 'undefined') {
-    exposedProperties.push(property);
     global[property] = document.defaultView[property];
   }
 });
